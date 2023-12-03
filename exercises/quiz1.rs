962b2cfec2e3cf03c4919f0c7e24fc0f5dc584dc
anderssortedal <Sortedal3@gmail.com>
2023-12-03 04:23:11 +0100

    initial sync

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples:i32) -> i32 {
+    if apples <= 0 {
+        return 0
+    }
+    if apples <= 40 {
+        return apples*2
+    }
+    return apples
+    
+} 
 
 // Don't modify this function!
 #[test]
